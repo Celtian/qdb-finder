41d3576400d49abad4a09f23115f1211cecf40be
Celtian <dominik.hladik@seznam.cz>
2026-07-19 12:37:59 +0200

    feat(header): menu changed

--- projects/electron/src/app/core/app-navigation-menu/app-navigation-menu.ts
+++ projects/electron/src/app/core/app-navigation-menu/app-navigation-menu.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-menu',
-  imports: [MatButtonModule, MatIconModule, MatMenuModule, RouterLink, RouterLinkActive],
+  imports: [MatIconModule, MatMenuModule, RouterLink, RouterLinkActive],
   templateUrl: './app-navigation-menu.html',
   styleUrl: './app-navigation-menu.css',
 })
